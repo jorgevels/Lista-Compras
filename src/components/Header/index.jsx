dc53b0aa1e43f0e401c11068cc0aba75f98958f3
jorgevels <jorgevelasquez006@hotmail.com>
2021-12-22 21:05:15 -0500

    update sw

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -37,7 +37,7 @@ const App = () => {
         </div> */}
         <Title>
           <Link to="/">
-            <p>Mercando2</p>
+            <p>Mercando6</p>
             {/*  <img className="Logo_header" src={Logo} alt="logo" /> */}
           </Link>
         </Title>
