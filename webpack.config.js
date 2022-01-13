7261b8152e5e979cec56ee1e80e24abdb18f4a62
jorgevels <jorgevelasquez006@hotmail.com>
2022-01-13 13:29:09 -0500

    Update

--- webpack.config.js
+++ webpack.config.js
@@ -74,7 +74,7 @@ module.exports = {
   },
 
   output: {
-    path: path.resolve(__dirname, "public"),
+    path: path.resolve(__dirname, "dist"),
     filename: "[name].bundle.js",
     chunkFilename: "[name].bundle.js",
     publicPath: "/",
